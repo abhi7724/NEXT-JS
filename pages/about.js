35d9f270d9db339f0ede8c964c203970ccc601a2
ABHINAV <abhinav30may@gmail.com>
2022-12-23 09:32:48 +0530

    designed the About Page

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import styles from "/styles/About.module.css";
 
 const about = () => {
   return (
-    <div>Kya haal hai</div>
+    <div className='{styles.container}'> 
+      <h1 className='{styles.center}'>About hunting Coder</h1>
+      <h2>Introduction</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, deserunt impedit aperiam architecto accusamus totam, obcaecati rerum fugiat aspernatur eaque molestias voluptatum. Necessitatibus, distinctio nihil! Quia saepe provident nulla magni quasi, inventore neque numquam reprehenderit aspernatur voluptas, deleniti assumenda esse nisi autem perspiciatis mollitia! Nemo, veritatis error? Repellat nisi nemo doloribus optio.</p>
+      <p>We offer the following services</p>
+      <ul>
+        <li>Service 1</li>
+        <li>Service 2</li>
+        <li>Service 3</li>
+        <li>Service 4</li>
+        <li>Service 5</li>
+        <li>Service 6</li>
+      </ul>
+      <h2>Services Offered</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit blanditiis tempora sunt at voluptate dolores voluptates ex asperiores laboriosam quaerat harum itaque impedit provident repudiandae amet commodi qui velit explicabo, quod perspiciatis culpa! Fugit.</p>
+      <h2>Contact US</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa repudiandae perferendis ut corporis odit, adipisci rem minus dolore dicta deleniti enim architecto illum? Quae quaerat blanditiis eius repellat neque alias cupiditate dignissimos pariatur quam repudiandae autem, sapiente fuga. Laboriosam quaerat modi ducimus earum magni, repellat qui quo repudiandae eaque eos rem facilis perspiciatis ad alias officiis!</p>
+    </div>
   )
 }
 
